c5b6c2d9ce4eb24aa16fe56803be8dd047b31123
Brick <724188767@qq.com>
2024-01-09 17:29:37 +0800

    try new caching method

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from fastai.vision import *
 from PIL import Image
 
 
-# @st.cache_data(max_entries=1)
+@st.cache_data
 def traditional_transform(img: Image):
     # 0.参数设置
     ksize = 15
@@ -61,7 +61,7 @@ def add_margin(pil_img, top, right, bottom, left, color):
     return result
 
 
-# @st.cache_data(max_entries=1)
+@st.cache_data
 def nn_transform(img: Image):
     im_temp = add_margin(img, 250, 250, 250, 250, (255, 255, 255))
     im_temp.save("temp.jpg", quality=95)
@@ -75,16 +75,27 @@ st.set_page_config(page_title="素描风格图像生成工具")
 st.title("素描风格图像生成工具")
 st.write("Made by brick and denny, grade 21, school of CSE, SCUT.")
 
-with st.spinner("正在加载中，请稍等……"):
+
+@st.cache_resource
+def get_model():
     MODEL_NAME = "ArtLine_920.pkl"
     # MODEL_NAME = "ArtLine_650.pkl"
     MODEL_URL = "https://www.dropbox.com/s/04suaimdpru76h3/ArtLine_920.pkl?dl=1"
     # MODEL_URL = "https://www.dropbox.com/s/starqc9qd2e1lg1/ArtLine_650.pkl?dl=1"
     if not os.path.exists(MODEL_NAME):
         urllib.request.urlretrieve(MODEL_URL, MODEL_NAME)
-    learn = load_learner(Path("."), MODEL_NAME)
-    exts = Image.registered_extensions().items()
-    supported_extensions = [f for ex, f in exts if f in Image.OPEN]
+    return load_learner(Path("."), MODEL_NAME)
+
+
+@st.cache_resource
+def get_extensions():
+    extensions = Image.registered_extensions().items()
+    return [f for ex, f in extensions if f in Image.OPEN]
+
+
+with st.spinner("正在加载中，请稍等……"):
+    learn = get_model()
+    supported_extensions = get_extensions()
 
 uploaded_file = st.file_uploader(
     "请上传一张待处理的图像:",
@@ -143,6 +154,6 @@ if print_no_image_error:
     st.error("不要着急，你需要先上传一张图片！")
 if print_nn_info:
     st.info("神经网络生成图像的不确定性可能会导致图像边缘有额外的白色区域。如有需要，可下载图像后自行截去。", icon="ℹ️")
-    # st.info(
-    #     "由于 streamlit 的机制，点击任意保存按钮后页面会刷新。针对这种情况我们进行了生成结果缓存处理，你只需再次点击生成按钮即可快速看到之前已经计算生成的结果。",
-    #     icon="ℹ️")
+    st.info(
+        "由于 streamlit 的机制，点击任意保存按钮后页面会刷新。针对这种情况我们进行了生成结果缓存处理，你只需再次点击生成按钮即可快速看到之前已经计算生成的结果。",
+        icon="ℹ️")
